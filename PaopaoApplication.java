5a0b96392f0e368eaa48d4a4e91df75f9d2be22c
Visen <zhvisen@gmail.com>
2018-12-17 22:24:03 +0800

    项目初始化

---
+++ PaopaoApplication.java
@@ -0,0 +1,14 @@
+package com.paopao.paopao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaopaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaopaoApplication.class, args);
+	}
+
+}
+
